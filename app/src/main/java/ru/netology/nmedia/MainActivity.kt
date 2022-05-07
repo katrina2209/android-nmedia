8ec14f3b9d134bed58d965733639dd354030add3
katrina2209 <katrina22@mail.ru>
2022-05-07 11:58:25 +0300

    MVVM

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,38 +2,35 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.annotation.DrawableRes
 import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.viewModel.PostViewModel
 import java.math.RoundingMode
 import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<PostViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 153642,
-            author = "Ekaterina",
-            content = "Hello everybody!",
-            published = "03.05.2022",
-            likes = 999,
-            shares = 0,
-            viewings = 15_555_555
-        )
 
-        binding.render(post)
+        viewModel.data.observe(this) { post ->
+            binding.render(post)
+        }
+
         binding.like.setOnClickListener {
-            post.likedByMe = !post.likedByMe
-            binding.like.setImageResource(getLikeIconResId(post.likedByMe))
-            binding.likesNumber.text = numberToString(getLikesCount(post))
-
+            viewModel.onLikeClicked()
         }
 
         binding.share.setOnClickListener {
-            binding.sharesNumber.text = numberToString(post.shares++)
+            viewModel.onShareClicked()
         }
     }
 
@@ -41,9 +38,9 @@ class MainActivity : AppCompatActivity() {
         author.text = post.author
         date.text = post.published
         content.text = post.content
-        likesNumber.text = numberToString(getLikesCount(post))
+        likesNumber.text = numberToString(post.likes)
         like.setImageResource(getLikeIconResId(post.likedByMe))
-        sharesNumber.text = numberToString(post.shares++)
+        sharesNumber.text = numberToString(post.shares)
         viewingsNumber.text = numberToString(post.viewings)
 
     }
@@ -52,8 +49,6 @@ class MainActivity : AppCompatActivity() {
     private fun getLikeIconResId(liked: Boolean) =
         if (liked) R.drawable.ic_liked_24dp else R.drawable.ic_favorite_24dp
 
-    private fun getLikesCount(post: Post) =
-        if (post.likedByMe) post.likes-- else post.likes++
 
     private fun numberToString(number: Int): String {
         val df = DecimalFormat("#.#")
@@ -62,8 +57,8 @@ class MainActivity : AppCompatActivity() {
         return when (number) {
             in 0..999 -> number.toString()
             in 1_000..9_999 -> "${df.format(num / 1000)}K"
-            in 10_000..999_999 -> "${Math.round(num / 100) / 10}K"
-            in 1_000_000..Integer.MAX_VALUE -> "${Math.round(num / 1_000_000)}M"
+            in 10_000..999_999 -> "${(num / 100).roundToInt() / 10}K"
+            in 1_000_000..Integer.MAX_VALUE -> "${(num / 1_000_000).roundToInt()}M"
             else -> "ERR"
         }
 
